6d8f82e1bff0802f371d8f1d587b1c0a596c769a
aliseena1 <aliseena@live.unc.edu>
2025-02-17 20:01:32 -0500

    Complete

--- src/quiz/promise-neg.ts
+++ src/quiz/promise-neg.ts
@@ -3,3 +3,29 @@ const array2D_3 = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+// Create a promise that goes through a given row and resolves the row if there is a negative found
+function negsPerRow(rowId: number): Promise<number[]> {
+    return new Promise((resolve, reject) => {
+        if (array2D_3.length > rowId) {
+                const hasNegative = array2D_3[rowId].some(num => num < 0);
+                resolve(hasNegative ? array2D_3[rowId] : []);
+        }
+        else {
+            reject('Row number must be valid');
+        }
+    });
+}
+
+// Concurrently call negsPerRow to efficiently find all rows with negative values then print them
+Promise.all([negsPerRow(0), negsPerRow(1), negsPerRow(2)]).then((results: number[][]) => {
+    let rows = []
+    for (const row of results) {
+        if (row.length > 0) {
+            rows.push(row)
+        }
+    }
+    console.log(rows);
+}).catch((error) => {
+    console.error(`Failed to calculate: ${error}`);
+});
--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -3,3 +3,27 @@ const array2D_1 = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+// Goes through every row and asynchronously finds the sum for that row
+async function sumRow(row: number): Promise<number> {
+    if (row >= array2D_1.length) {
+        throw new Error('Must provide a valid row');
+    }
+    return array2D_1[row].reduce((sum, num) => sum + num, 0);
+}
+
+// Calculate the total sum by calling sumRow on each row of the array
+async function getTotalSum() {
+    try {
+        const firstRow = await sumRow(0);
+        const secondRow = await sumRow(1);
+        const thirdRow = await sumRow(2);
+        console.log(firstRow + secondRow + thirdRow)
+        return firstRow + secondRow + thirdRow;
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+getTotalSum();
